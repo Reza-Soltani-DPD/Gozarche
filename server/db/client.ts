5ef2155ad7b951249fac9370e918d8d1a1d0d7a4
Reza-Soltani-DPD <reza.soltani.dpd@gmail.com>
2022-12-29 14:32:10 +0330

    db schema change

--- server/db/client.ts
+++ server/db/client.ts
@@ -1,23 +1,23 @@
 import {prisma as cl} from './db'
 
-cl.$use(async (params,next)=>{
-  try{
-    if(params.model=="dbchanges" && !["create","createMany","updata","updataMany","delete","deleteMany","upsert"].includes(params.action)){
-      return await next(params)
-    }else{
-       await cl.dbchanges.create({data:{
-        argdata: JSON.stringify(params.args),
-        actionName:params.action,
-        modelName:params.model,
+// cl.$use(async (params,next)=>{
+//   try{
+//     if(params.model=="dbchanges" && !["create","createMany","updata","updataMany","delete","deleteMany","upsert"].includes(params.action)){
+//       return await next(params)
+//     }else{
+//        await cl.dbchanges.create({data:{
+//         argdata: JSON.stringify(params.args),
+//         actionName:params.action,
+//         modelName:params.model,
         
-      }})
-      const result =await next(params)
-      return result
+//       }})
+//       const result =await next(params)
+//       return result
 
-    }
-  }
-  catch(err){
-  }
-})
+//     }
+//   }
+//   catch(err){
+//   }
+// })
 
 export const prisma =cl
