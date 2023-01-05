3225c9c91ff893caa94058d6a9ac931763f52f26
Reza-Soltani-DPD <reza.soltani.dpd@gmail.com>
2023-01-05 22:21:31 +0330

    database final change

--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -1,41 +1,48 @@
 import React from "react";
 
-import { Navigation} from 'swiper';
+import SwiperCore, { Navigation } from "swiper";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/navigation";
 
-
-
-
-
-
+SwiperCore.use([Navigation])
 export default function Carousel() {
+  const navigationPrevRef = React.useRef<HTMLDivElement>(null);
+  const navigationNextRef = React.useRef<HTMLDivElement>(null);
+  
+  const onBeforeInit = (Swiper: SwiperCore): void => {
+    if (typeof Swiper.params.navigation !== 'boolean' &&Swiper.params.navigation) {
+      const navigation = Swiper.params.navigation;
+      navigation.prevEl = navigationPrevRef.current;
+      navigation.nextEl = navigationNextRef.current;
+    }
 
-
+  };
   return (
     <>
-   <Swiper
-      // install Swiper modules
-      modules={[Navigation]}
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
+      <Swiper
+      onBeforeInit={onBeforeInit}
+        navigation={{
+          prevEl: navigationPrevRef.current,
+          nextEl: navigationNextRef.current
+        }}
 
-    >
-      <SwiperSlide><div className='flex justify-center'>hello</div></SwiperSlide>
-      <SwiperSlide><div className='flex justify-center'>hello</div></SwiperSlide>
-      <SwiperSlide><div className='flex justify-center'>hello</div></SwiperSlide>
-      <SwiperSlide><div className='flex justify-center'>hello</div></SwiperSlide>
-      <SwiperSlide><div className='flex justify-center'>hello</div></SwiperSlide>
-      <SwiperSlide><div className='flex justify-center'>hello</div></SwiperSlide>
-      <SwiperSlide><div className='flex justify-center'>hello</div></SwiperSlide>
-      <SwiperSlide><div className='flex justify-center'>hello</div></SwiperSlide>
-    
-      ...
-    </Swiper>
+        modules={[Navigation]}
+        spaceBetween={8}
+        slidesPerView={3}
+      >
+        {[...Array(100).keys()].map((number)=>{
+        return(
+        <SwiperSlide key={number}>
+          <div className="flex justify-center bg-black text-rose-600 h-48">{number}</div>
+        </SwiperSlide>
+        ) 
+        })}
+        <div ref={navigationPrevRef}>perv</div>
+        <div ref={navigationNextRef}>next</div>
+      </Swiper>
     </>
   );
 }
